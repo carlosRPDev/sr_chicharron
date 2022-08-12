ec07d51c9d746f86058566610de1db0e48f5d7a3
Carlos Rodriguez <ucroxlive@gmail.com>
2022-08-12 02:23:30 -0500

    Se utiliza estimulus para la funcionalidad de los toast

---
+++ app/javascript/controllers/toast_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="toast"
+export default class extends Controller {
+  connect() {
+  }
+
+  notify() {
+    const toastLiveExample = document.getElementById('liveToast')
+    const toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  }
+}
